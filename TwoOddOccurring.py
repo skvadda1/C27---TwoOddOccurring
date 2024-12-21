b8119b33907c28934912d26210008bdb4c6f2382
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-12-21 10:11:31 -0500

    Add files via upload

---
+++ TwoOddOccurring.py
@@ -0,0 +1,20 @@
+def TwoOdd(arr, size):
+    xorof2 = arr[0]
+    x = 0
+    y = 0
+    for i in range(1,size):
+        xorof2 = xorof2 ^ arr[i]
+    SetBit = xorof2 & ~(xorof2 - 1)
+    for i in range(size):
+        if(arr[i] & SetBit):
+            x = x ^ arr[i]
+        else:
+            y = y ^ arr[i]
+    print("Two odd elements are",x ,"&", y)
+arr = []
+arr_size = int(input("Enter size of array:"))
+for i in range(0,arr_size):
+    z = int(input("Enter element:"))
+    arr.append(z)
+print("Two Odd")
+
